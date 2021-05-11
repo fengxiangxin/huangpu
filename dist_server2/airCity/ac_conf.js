57d1a75089eb62395c04166240cc9aff3aa69a06
fengxiangxin <feng_xiangxin@qq.com>
2021-05-11 11:19:42 +0800

    -

--- dist_server2/airCity/ac_conf.js
+++
@@ -1,7 +0,0 @@
-var HostConfig = {
-  "MatchServer": "10.140.241.144:83",
-  "AirCityPlayer": "10.140.241.144:8081",
-  "AirCityAPI": "10.140.241.144:4323",
-  "Token": "",
-  "Path": "D:\\AirCityCloud2021.0420_Trunk\\SDK"
-}
