3e3656aa6240872ba56957df807c886b502dbdf1
fengxiangxin <feng_xiangxin@qq.com>
2021-05-19 16:42:28 +0800

    -

---
+++ src/views/Tree/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="tree"></div>
+</template>
+
+<script>
+export default {
+  name: "Tree",
+  data() {
+    return {};
+  },
+  methods: {
+      
+  },
+  mounted() {},
+  beforeDestroy() {},
+  components: {},
+};
+</script>
+
+<style lang='less' scoped>
+</style>
